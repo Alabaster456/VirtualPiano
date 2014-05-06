0e273633b065258929d62ebfba7605c77d71856a
Yazid <Alabaster456@users.noreply.github.com>
2014-05-06 10:18:36 -0700

    Create jQuery.js

---
+++ jQuery.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+  $('#soundA').click(function(){
+    $('#soundA').fadeTo("fast", 0.2);
+    $('#soundA').fadeTo("slow",1);
+    var pianoA = new Audio("https://www.freesound.org/people/pinkyfinger/sounds/68437/download/68437__pinkyfinger__piano-a.wav");
+    pianoA.play();
+  });
+  $('#soundB').click(function(){
+    $('#soundB').fadeTo("fast",0.2);
+    $('#soundB').fadeTo("slow",1);
+    var pianoB = new Audio("https://www.freesound.org/people/pinkyfinger/sounds/68438/download/68438__pinkyfinger__piano-b.wav");
+    pianoB.play();
+  });
+  $('#soundC').click(function(){
+    
+     $('#soundC').fadeTo("fast",0.2);
+    $('#soundC').fadeTo("slow",1);
+    var pianoC = new Audio("https://www.freesound.org/people/pinkyfinger/sounds/68441/download/68441__pinkyfinger__piano-c.wav");
+    pianoC.play();
+  });
+ $('#soundD').click(function(){ 
+    $('#soundD').fadeTo("fast",0.2);
+    $('#soundD').fadeTo("slow",1);
+   var pianoD = new Audio("https://www.freesound.org/people/pinkyfinger/sounds/68442/download/68442__pinkyfinger__piano-d.wav");
+                              pianoD.play();
+  
+                             }); 
+$('#soundG').click(function(){
+   $('#soundG').fadeTo("fast",0.2);
+    $('#soundG').fadeTo("slow",1);
+  
+
+
+  var pianoG = new Audio(" https://www.freesound.org/people/pinkyfinger/sounds/68448/download/68448__pinkyfinger__piano-g.wav");
+  pianoG.play();
+});
+
+  $('#soundE').click(function(){
+    $('#soundE').fadeTo("fast",0.2);
+    $('#soundE').fadeTo("slow",1);
+    var pianoE = new Audio("https://www.freesound.org/people/pinkyfinger/sounds/68443/download/68443__pinkyfinger__piano-e.wav");
+  pianoE.play();
+  });
+  $('#soundF').click(function(){
+$('#soundF').fadeTo("fast",0.2);
+    $('#soundF').fadeTo("slow",1);
+    var pianoF = new Audio("https://www.freesound.org/people/pinkyfinger/sounds/68446/download/68446__pinkyfinger__piano-f.wav");
+    pianoF.play();
+  });
+  $('#soundGnum').click(function(){
+  $('#soundGnum').fadeTo("fast",0.2);
+    $('#soundGnum').fadeTo("slow",1);
+    var pianoG1 = new Audio("https://www.freesound.org/people/pinkyfinger/sounds/68447/download/68447__pinkyfinger__piano-g.wav");
+    pianoG1.play();
+  });
+  $('#soundFnum').click(function(){
+ $('#soundFnum').fadeTo("fast",0.2);
+    $('#soundFnum').fadeTo("slow",1);
+    var pianoF1 = new Audio("https://www.freesound.org/people/pinkyfinger/sounds/68445/download/68445__pinkyfinger__piano-f.wav");
+    pianoF1.play();
+  });
+  $('#soundCnum').click(function(){
+                    $('#soundCnum').fadeTo("fast",0.2);
+  $('#soundCnum').fadeTo("slow",1);
+  var pianoC1 = new Audio("https://www.freesound.org/people/pinkyfinger/sounds/68440/download/68440__pinkyfinger__piano-c.wav");
+  pianoC1.play();
+                        });
+  $('#soundBb').click(function(){
+    $('#soundBb').fadeTo("fast",0.2);
+  $('#soundBb').fadeTo("slow",1);
+    var pianoBB = new Audio("https://www.freesound.org/people/pinkyfinger/sounds/68439/download/68439__pinkyfinger__piano-bb.wav");
+    pianoBB.play();
+    
+  });
+  $("#soundeB").click(function(){
+      $('#soundeB').fadeTo("fast",0.2);
+  $('#soundeB').fadeTo("slow",1);
+  
+  var pianoeB = new Audio("https://www.freesound.org/people/pinkyfinger/sounds/68444/download/68444__pinkyfinger__piano-eb.wav");
+  pianoeB.play();
+  });
+  $("#soundanon").click(function(){
+    $('#soundanon').fadeTo("fast",0.2);
+  $('#soundanon').fadeTo("slow",1);
+  var pianoanon = new Audio("http://soundjax.com/reddo/22641%5Eg-piano4.mp3");
+    pianoanon.play();
+  });
+  $("#soundanony").click(function(){
+    $("#soundanony").fadeTo("fast",0.2);
+    $("#soundanony").fadeTo("slow",1);
+    var pianoanon1 = new Audio("http://soundjax.com/reddo/85962%5Eg-piano3.mp3");
+    pianoanon1.play();
+  });
+  $("#soundanon2").click(function(){
+    $("#soundanon2").fadeTo("fast",0.2);
+    $("#soundanon2").fadeTo("slow",1);
+    var pianoanon2 = new Audio("http://soundjax.com/reddo/92427%5Epiano_A1.mp3");
+    pianoanon2.play();
+  });
+  $("#soundanon3").click(function(){
+    $("#soundanon3").fadeTo("fast",0.2);
+   $("#soundanon3").fadeTo("slow",1);
+    var pianoanonm = new Audio("http://soundjax.com/reddo/39022%5EPianoNote.mp3");
+    pianoanonm.play();
+  });
+  });
